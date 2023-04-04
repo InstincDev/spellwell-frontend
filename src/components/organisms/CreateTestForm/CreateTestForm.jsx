00d92d7223473af4d3b695995a0f753f29123d5a
InstincDev <crene7722@gmail.com>
2023-04-04 16:57:39 -0500

    validated base words and sentences

--- src/components/organisms/CreateTestForm/CreateTestForm.jsx
+++ src/components/organisms/CreateTestForm/CreateTestForm.jsx
@@ -1,30 +1,161 @@
 import { useState } from "react";
 import { AddWords } from "../../";
-
+/*
+    TODO:
+        refactor
+        validate - base and challenge array length
+*/
 export function CreateTestForm() {
     const handleSubmit = (event) => {
         event.preventDefault();
     };
-
-    const [basewords, setBasewords] = useState([]);
-    const [challengewords, setChallengewords] = useState([]);
+    /*
+    Data structure for Base & Challenge
+    [{word: "", sentence: ""}]
+    */
+    const [baseWordLists, setBaseWordLists] = useState([]);
+    const [challengeWordLists, setChallengeWordLists] = useState([]);
 
     return (
         <form onSubmit={handleSubmit}>
-            <h2>Base Words</h2>
-            <AddWords
-                label={"baseword"}
-                type={"text"}
-                addWord={(word) => setBasewords([...basewords, word])}
-            />
+            <h2>Base</h2>
+            <BaseWordsInput />
+            <h2>Challenge</h2>
+            <ChallengeWordsInput />
 
-            <h2> Challenge</h2>
-            <AddWords
-                label={"challengeword"}
-                type={"text"}
-                addWord={(word) => setChallengewords([...challengewords, word])}
-            />
-            <button type="submit">Submit</button>
+            <button type="submit">Form Submit</button>
         </form>
     );
+}
+
+/* TODO:
+    validation
+*/
+function BaseWordsInput() {
+    const [word, setWord] = useState("");
+    const [sentence, setSentence] = useState("");
+    const [submittedWordInputError, setSubmittedWordInputError] =
+        useState(null);
+    const [submittedSentenceInputError, setSubmittedSentenceInputError] =
+        useState(null);
+    // no numbers or symbols - only alphas
+    // != ""
+    // convert .split("").filter(!=" ").join("").toLowerCase()
+    const handleWordOnSubmit = () => {
+        if (word === "" || sentence === "") {
+            word === "" && setSubmittedWordInputError("Empty Word");
+            sentence === "" && setSubmittedSentenceInputError("Empty Sentence");
+            return;
+        }
+
+        {
+            word.split("").forEach((e) => {
+                console.log(e);
+                if (Number.isInteger(parseInt(e)))
+                    setSubmittedWordInputError("Cannot Contain Numbers");
+            });
+
+            sentence.split("").forEach((e) => {
+                console.log(e);
+                if (Number.isInteger(parseInt(e)))
+                    setSubmittedSentenceInputError("Cannot Contain Numbers");
+            });
+        }
+
+        // const cleanSentence = sentence.trim()
+        // const cleanWord = word.split("").filter(e => e !== ' ').join("").toLowerCase()
+    };
+
+    const handleWordOnChange = (e) => {
+        const value = e.target.value;
+        setSubmittedWordInputError(null);
+        setWord(value);
+    };
+
+    const handleSentenceOnChange = (e) => {
+        const value = e.target.value;
+        setSubmittedSentenceInputError(null);
+        setSentence(value);
+    };
+
+    return (
+        <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+            <label htmlFor="word">
+                {submittedWordInputError && (
+                    <InputToolTip message={submittedWordInputError} />
+                )}
+                <input
+                    type="text"
+                    name="word"
+                    id="word"
+                    placeholder="Base Word"
+                    value={word}
+                    onChange={handleWordOnChange}
+                />
+            </label>
+            <label htmlFor="sentence">
+                {submittedSentenceInputError && (
+                    <InputToolTip message={submittedSentenceInputError} />
+                )}
+                <input
+                    type="text"
+                    name="sentence"
+                    id="sentence"
+                    placeholder="Base Word Sentence"
+                    value={sentence}
+                    onChange={handleSentenceOnChange}
+                />
+            </label>
+            <button type="button" onClick={handleWordOnSubmit}>
+                Base Submit
+            </button>
+        </div>
+    );
+}
+
+/*
+    TODO:
+        validation
+
+*/
+
+function ChallengeWordsInput() {
+    const [challengeWord, setChallengeWord] = useState("");
+    const [challengeSentence, setChallengeSentence] = useState("");
+    const handleChallengeWordOnChange = (e) => {
+        const value = e.target.value;
+        setChallengeWord(value);
+    };
+    const handleChallengeSentenceOnChange = (e) => {
+        const value = e.target.value;
+        setChallengeSentence(value);
+    };
+    return (
+        <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+            <label htmlFor="challengeWord">
+                <input
+                    type="text"
+                    name="challengeWord"
+                    id="challengeWord"
+                    placeholder="challengeWord"
+                    value={challengeWord}
+                    onChange={handleChallengeWordOnChange}
+                />
+            </label>
+            <label htmlFor="challengeSentence">
+                <input
+                    type="text"
+                    name="challengeSentence"
+                    id="challengeSentence"
+                    placeholder="challengeSentence"
+                    value={challengeSentence}
+                    onChange={handleChallengeSentenceOnChange}
+                />
+            </label>
+        </div>
+    );
+}
+
+function InputToolTip({ message }) {
+    return <span>{message}</span>;
 }
