1d1f71a8c1a8cab95fa28e4971ae0eaeafc914ba
Winston <98111187+WinstonHall@users.noreply.github.com>
2023-04-22 09:58:55 -0400

    Update WordInput.jsx

--- src/components/organisms/WordInput/WordInput.jsx
+++ src/components/organisms/WordInput/WordInput.jsx
@@ -2,6 +2,12 @@ import { useState } from "react";
 import { InputToolTip } from "../../atoms";
 import { WordList } from "../../molecules";
 
+/* 
+    TODO:
+        -Find where state logic is being changed but not updated in our handleSubmit function
+        -Figure out weather there is a re-render or race condition, when setting state and updating, that might be caused by a race condition.
+*/
+
 export function WordInput({ 
     title,
     formWordError,
@@ -9,26 +15,34 @@ export function WordInput({
     addWord,
     deleteWord
       }) {
+    
+    // INPUT STATE
     const [word, setWord] = useState("");
     const [sentence, setSentence] = useState("");
-    const [wordInputError, setWordInputError] =
-        useState(null);
-    const [sentenceInputError, setSentenceInputError] =
-        useState(null);
-    // no numbers or symbols - only alphas
-    // != ""
-    // convert .split("").filter(!=" ").join("").toLowerCase()
+    // INPUT ERROR STATE
+    const [wordInputError, setWordInputError] = useState(null);
+    const [sentenceInputError, setSentenceInputError] = useState(null);
+
+    
     const handleWordOnSubmit = () => {
-        if (word === "" || sentence === "") {
+        // Check word and sentence (inputs)
+        // Set error state for either/both that have an error
+        // Then short circut if the above errored
+        if (word === "" || sentence === "")
             word === "" && setWordInputError("Empty Word");
             sentence === "" && setSentenceInputError("Empty Sentence");
             return;
         }
-
+        
+        // Make word and sentence an array of each char from their strings
         let splitWord = word.split("");
         let splitSentence = sentence.split("");
-
+        
+        // Loop both the arrays above and check for int chars
+        // In each look if they find an int they will set an error then break the loop
+        // If either error is not equal to their default value(null) then short circut
         {
+            // SPLIT WORD
             for (let i = 0; i < splitWord.length; i++) {
                 if (Number.isInteger(parseInt(splitWord[i]))) {
                     setWordInputError(
@@ -37,6 +51,7 @@ export function WordInput({
                     break;
                 }
             }
+            // SPLIT SENTENCE
             for (let i = 0; i < splitSentence.length; i++) {
                 if (Number.isInteger(parseInt(splitSentence[i]))) {
                     setSentenceInputError(
@@ -45,24 +60,33 @@ export function WordInput({
                     break;
                 }
             } 
-
+            
            if (
             wordInputError !== null ||
             sentenceInputError !== null
         )
             return;
         }
-
+        
+        // Filter word empty strings, then join, then set to lowercase
         splitWord = splitWord
         .filter((e) => e != " ")
         .join("")
         .toLowerCase();
+        
+        // Join sentence and trim the front and end of empty strings
         splitSentence = splitSentence.join("").trim();
 
-        addWord({ word: splitWord, sentence: splitSentence });
-
-        setWord("");
-        setSentence("");
+        // handleSubmit made it this far without a short circut.
+        // If all else fails and we cant get the short circut to work
+        // we can always throw a check statement here to only run this if errors are null.
+        {
+            // Add word struct to word list/array
+            // Then reset the work inputs to empty
+            addWord({ word: splitWord, sentence: splitSentence });
+            setWord("");
+            setSentence("");
+        }
     };
 
     const handleWordOnChange = (e) => {
