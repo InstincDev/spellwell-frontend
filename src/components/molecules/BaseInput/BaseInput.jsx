4028adb864c82f16050af54a48c85706b55fdbcb
InstincDev <crene7722@gmail.com>
2023-06-21 18:43:02 -0500

    updated and recreated reusable  input components for form validation

--- src/components/molecules/BaseInput/BaseInput.jsx
+++ src/components/molecules/BaseInput/BaseInput.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
 import { form_label } from "../../../styling/form.module.sass";
 import userInputEnums from "../../../utils/userInputEnums";
+import { InputToolTip } from "../../atoms";
 
-export function BaseInput({
-    name,
-    type,
-    placeholder,
-    onChange,
-    value,
-}) {
+// ToDo
+    // reset tooltip state when user corrects errors
+    //  refactor other form inputs
+
+
+export function BaseInput({ name, type, placeholder, onChange, value }) {
     const [checkPassword, setCheckPassword] = useState(false);
+    const [isValid, setIsValid] = useState(true)
+    const [toolTipMessage, setToolTipMessage] = useState(null)
+
+    const handleInvalid = (e) => {
+        const input = e.target
+        
+        // disable customValidity tooltip
+            // input.setCustomValidity("Nope")
+
+        setIsValid(input.validity.valid)
+        console.log(isValid);
+        setToolTipMessage(input.validationMessage)
+    };
 
     return (
         <label htmlFor={name} className={form_label}>
-            
             <input
                 type={!checkPassword ? type : "text"}
                 name={name}
@@ -21,9 +33,18 @@ export function BaseInput({
                 placeholder={placeholder}
                 onChange={onChange}
                 value={value}
-                minLength={name == userInputEnums.USER_PASSWORD || name == userInputEnums.USER_CONFIRM_PASSWORD? "8": "5" }
+                minLength={
+                    name == userInputEnums.USER_PASSWORD ||
+                    name == userInputEnums.USER_CONFIRM_PASSWORD
+                        ? "8"
+                        : "5"
+                }
                 required
+                onInvalid={handleInvalid}
+
             />
+
+            {isValid !==true && <InputToolTip message={toolTipMessage}/>}
             {/* {type == "password" && (
                 <button
                     onClick={() => {
--- src/components/organisms/CreateUserForm/CreateUserForm.jsx
+++ src/components/organisms/CreateUserForm/CreateUserForm.jsx
@@ -5,85 +5,101 @@
 // class id
 
 import { useState } from "react";
-import {create_test_form, form_head, form_label, form_button, base_input_wrapper} from '../../../styling/form.module.sass'
-import {BaseInput} from '../../'
+import {
+    create_test_form,
+    form_head,
+    form_label,
+    form_button,
+    base_input_wrapper,
+} from "../../../styling/form.module.sass";
+import { BaseInput } from "../../";
 import userInputEnums from "../../../utils/userInputEnums";
 
-
-
-export function CreateUserForm(){
-    const [userName, setUserName] = useState("")
-    const [userEmail, setUserEmail] = useState("")
-    const [userPassword, setUserPassword] = useState("")
-    const [confirmPassword, setConfirmPassword] = useState("")
+export function CreateUserForm() {
+    const [userName, setUserName] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
     // const [userClassId, setUserClassId] = useState("")
-    const [userType, setUserType] = useState("select")
+    const [userType, setUserType] = useState("select");
 
-    function handleSubmit(e){
-        e.preventDefault()
+    function handleSubmit(e) {
+        e.preventDefault();
         // Smile
         // state error handling
-            // all exist
+        // all exist
         // package state values
         // fetch to backend with data
 
-
         console.log("Submitted");
     }
-    
-    function handleInput(e){
-        const {name, value} = e.target
-        switch(name){
-            case userInputEnums.USER_NAME: setUserName(value)
+
+    function handleInput(e) {
+        const { name, value } = e.target;
+        switch (name) {
+            case userInputEnums.USER_NAME:
+                setUserName(value);
                 break;
-            case userInputEnums.USER_EMAIL: setUserEmail(value)
+            case userInputEnums.USER_EMAIL:
+                setUserEmail(value);
                 break;
-            case userInputEnums.USER_PASSWORD: setUserPassword(value)
+            case userInputEnums.USER_PASSWORD:
+                setUserPassword(value);
                 break;
-            case userInputEnums.USER_CONFIRM_PASSWORD: setConfirmPassword(value)
+            case userInputEnums.USER_CONFIRM_PASSWORD:
+                setConfirmPassword(value);
                 break;
-            case userInputEnums.USER_TYPE: setUserType(value)
+            case userInputEnums.USER_TYPE:
+                setUserType(value);
                 break;
-            default: console.log("handle input error");
+            default:
+                console.log("handle input error");
         }
     }
 
-    return(
+    return (
         <form onSubmit={handleSubmit} className={create_test_form}>
-            <h3 className={form_head}>
-                User Login
-            </h3>
+            <h3 className={form_head}>User Login</h3>
 
             <div className="user_login_type">
-                <select name={userInputEnums.USER_TYPE} id="" onChange={handleInput} value={userType}>
-                    <option value="select" aria-disabled >Select</option>
+                <select
+                    name={userInputEnums.USER_TYPE}
+                    id=""
+                    onChange={handleInput}
+                    value={userType}
+                >
+                    <option value="select" aria-disabled hidden>
+                        Select
+                    </option>
                     <option value="teacher">Teacher</option>
                     <option value="student">Student</option>
                     <option value="parent">Parent</option>
                 </select>
             </div>
             <div className={base_input_wrapper}>
-
                 <BaseInput
                     className={form_label}
                     type="text"
                     name={userInputEnums.USER_NAME}
                     placeholder="Full Name"
-                    onChange={handleInput} value={userName}
+                    onChange={handleInput}
+                    value={userName}
                 />
                 <BaseInput
                     className={form_label}
                     type="email"
                     name={userInputEnums.USER_EMAIL}
                     placeholder="Email"
-                    onChange={handleInput} value={userEmail}
+                    onChange={handleInput}
+                    value={userEmail}
                 />
                 <BaseInput
                     className={form_label}
                     type="password"
                     name={userInputEnums.USER_PASSWORD}
                     placeholder="Password"
-                    onChange={handleInput} value={userPassword}
+                    onChange={handleInput}
+                    value={userPassword}
                 />
 
                 <BaseInput
@@ -91,7 +107,8 @@ export function CreateUserForm(){
                     type="password"
                     name={userInputEnums.USER_CONFIRM_PASSWORD}
                     placeholder="Confirm Password"
-                    onChange={handleInput} value={confirmPassword}
+                    onChange={handleInput}
+                    value={confirmPassword}
                 />
                 {/* <BaseInput
                     className={form_label}
@@ -101,8 +118,10 @@ export function CreateUserForm(){
                     onChange={handleInput} value={userClassId}
                 /> */}
             </div>
-           
-            <button type="submit" className={form_button}>Login</button>
+
+            <button type="submit" className={form_button}>
+                Login
+            </button>
         </form>
-    )
+    );
 }
